cd92943968b4169848f96fb22885b4d2e2250f63
y-takagi <ytakagi.deb@gmail.com>
2017-05-24 16:29:48 +0900

    Modify to use tide and typescript-mode.

---
+++ inits/80_tide.el
@@ -0,0 +1,9 @@
+(use-package tide
+  :config
+  (add-hook 'typescript-mode-hook
+            (lambda ()
+              (tide-setup)
+              (flycheck-mode t)
+              (setq flycheck-check-syntax-automatically '(save mode-enabled))
+              (eldoc-mode t)))
+  )
