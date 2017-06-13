fd74f20063b735475a489dd93dab355a4038e47f
y-takagi <ytakagi.deb@gmail.com>
2017-06-14 03:17:59 +0900

    Add typescript-tslint exec-path.

--- inits/40_flycheck.el
+++ inits/40_flycheck.el
@@ -9,5 +9,17 @@
                                           root))))
       (when (and eslint (file-executable-p eslint))
         (setq-local flycheck-javascript-eslint-executable eslint))))
+
+  (defun my/use-tslint-from-node-modules ()
+    (let* ((root (locate-dominating-file
+                  (or (buffer-file-name) default-directory)
+                  "node_modules"))
+           (tslint (and root
+                        (expand-file-name "node_modules/.bin/tslint"
+                                          root))))
+      (when (and tslint (file-executable-p tslint))
+        (setq-local flycheck-typescript-tslint-executable tslint))))
+
   (add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
+  (add-hook 'flycheck-mode-hook #'my/use-tslint-from-node-modules)
   )
