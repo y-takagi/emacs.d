1e89170f94e8b5fb6c6db742d8757dd7c4dfcd57
y-takagi <ytakagi.deb@gmail.com>
2019-01-04 22:00:16 +0900

    custom.el changed

--- custom.el
+++ custom.el
@@ -18,3 +18,9 @@
  '(rainbow-html-colors-major-mode-list
    (quote
     (scss-mode html-mode css-mode php-mode nxml-mode xml-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
