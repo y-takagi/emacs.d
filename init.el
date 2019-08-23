47d3b7e37eb20ded9f70a2c6a6b1e32ff1b37611
y-takagi <ytakagi.deb@gmail.com>
2019-08-23 18:11:14 +0900

    Change place of global setting code.

--- init.el
+++ init.el
@@ -5,98 +5,6 @@
 
 ;;(add-to-list 'load-path (concat user-emacs-directory "site-lisp"))
 
-;;; language and coding
-(prefer-coding-system 'utf-8-unix)
-;; (set-language-environment "Japanese")
-;; (setq file-name-coding-system 'utf-8)
-;; (setq locale-coding-system 'utf-8)
-
-(when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-(blink-cursor-mode 1)
-(show-paren-mode 1) ;; highlight corresponding bracket
-(setq inhibit-startup-message t)
-(setq ring-bell-function 'ignore)
-(setq vc-follow-symlinks t)
-(setq-default cursor-in-non-selected-windows nil)
-
-;; line number
-(add-hook 'prog-mode-hook #'display-line-numbers-mode)
-(add-hook 'text-mode-hook #'display-line-numbers-mode)
-(add-hook 'conf-space-mode-hook #'display-line-numbers-mode)
-(add-hook 'conf-unix-mode-hook #'display-line-numbers-mode)
-(add-hook 'conf-toml-mode-hook #'display-line-numbers-mode)
-
-;;; disable auto indent
-(electric-indent-mode -1)
-
-;;; exitコマンド
-(defalias 'exit 'save-buffers-kill-terminal)
-
-;;; 変更されたファイルを自動的に再読み込み
-(global-auto-revert-mode 1)
-
-;;; tab
-(setq-default tab-width 4 indent-tabs-mode nil)
-(setq-default basic-offset 2)
-(setq-default c-basic-offset 2)
-
-;; indent
-(setq css-indent-offset 2)
-(setq js-indent-level 2)
-(setq python-indent-guess-indent-offset nil)
-
-;;; file
-(setq mode-require-final-newline t)
-(setq backup-inhibited t)
-(setq delete-auto-save-files t)
-
-;;; 問い合せには y か n で返答
-(fset 'yes-or-no-p 'y-or-n-p)
-
-;;; emacs permission
-;; (setq wdired-allow-to-change-permissions t)
-
-;;; diredの表示オプション
-(if (executable-find "gls")
-    (progn
-      (setq insert-directory-program "gls")
-      (setq dired-listing-switches "-ahl --time-style long-iso --group-directories-first"))
-  (setq dired-listing-switches "-ahl --time-style long-iso --group-directories-first"))
-
-;;; emacs のデフォルトブラウザを eww に変更
-(setq browse-url-browser-function 'eww-browse-url)
-
-;;; Global Key
-;; copy & paste
-(global-set-key "\M-w" 'clipboard-kill-ring-save)
-(global-set-key "\C-w" 'clipboard-kill-region)
-
-(global-set-key "\C-cc"    'comment-or-uncomment-region)
-(global-set-key "\C-h"     'backward-delete-char)
-(global-set-key "\C-x\C-i" 'indent-region)
-(global-set-key [C-tab]    'indent-for-tab-command)
-
-(global-unset-key "\C-x\C-z")
-(global-unset-key "\C-o")
-
-(when window-system
-  ;; Fonts
-  (add-to-list 'default-frame-alist '(font . "ricty-14"))
-
-  ;; フルスクリーン
-  (add-hook 'emacs-startup-hook #'toggle-frame-maximized)
-
-  ;; Modify right command to super
-  (setq mac-right-command-modifier 'super)
-
-  ;; フォントの拡大・縮小
-  (global-set-key (kbd "s-=") (lambda () (interactive) (text-scale-increase 1)))
-  (global-set-key (kbd "s--") (lambda () (interactive) (text-scale-decrease 1)))
-  (global-set-key (kbd "s-0") (lambda () (interactive) (text-scale-increase 0)))
-  )
-
 ;; package.el settings
 (require 'package)
 (package-initialize)
@@ -378,7 +286,6 @@
   (setq open-junk-file-format "~/Dropbox/Note/junk/%Y-%m%d-%H%M%S."))
 (use-package org
   :ensure t
-  :bind (("C-o c" . org-capture))
   :config
   (setq org-directory "~/Dropbox/Note/org/")
   (setq org-default-notes-file (concat org-directory "default.org"))
@@ -386,6 +293,8 @@
   (setq org-project-file (concat org-directory "project-wtvr.org"))
 
   ;; keybind
+  (global-unset-key "\C-o")
+  (global-set-key "\C-oc" 'org-capture)
   (global-set-key "\C-ca" 'org-agenda)
   (global-set-key "\C-cl" 'org-store-link)
   (global-set-key "\C-cb" 'org-iswitchb)
@@ -534,6 +443,96 @@
 (use-package yagist :ensure t)
 (use-package yaml-mode :ensure t)
 (use-package yasnippet :ensure t)
+
+;;; language and coding
+(prefer-coding-system 'utf-8-unix)
+;; (set-language-environment "Japanese")
+;; (setq file-name-coding-system 'utf-8)
+;; (setq locale-coding-system 'utf-8)
+
+(when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(blink-cursor-mode 1)
+(show-paren-mode 1) ;; highlight corresponding bracket
+(setq inhibit-startup-message t)
+(setq ring-bell-function 'ignore)
+(setq vc-follow-symlinks t)
+(setq-default cursor-in-non-selected-windows nil)
+
+;; line number
+(add-hook 'prog-mode-hook #'display-line-numbers-mode)
+(add-hook 'text-mode-hook #'display-line-numbers-mode)
+(add-hook 'conf-space-mode-hook #'display-line-numbers-mode)
+(add-hook 'conf-unix-mode-hook #'display-line-numbers-mode)
+(add-hook 'conf-toml-mode-hook #'display-line-numbers-mode)
+
+;;; disable auto indent
+(electric-indent-mode -1)
+
+;;; exitコマンド
+(defalias 'exit 'save-buffers-kill-terminal)
+
+;;; 変更されたファイルを自動的に再読み込み
+(global-auto-revert-mode 1)
+
+;;; tab
+(setq-default tab-width 4 indent-tabs-mode nil)
+(setq-default basic-offset 2)
+(setq-default c-basic-offset 2)
+
+;; indent
+(setq css-indent-offset 2)
+(setq js-indent-level 2)
+(setq python-indent-guess-indent-offset nil)
+
+;;; file
+(setq mode-require-final-newline t)
+(setq backup-inhibited t)
+(setq delete-auto-save-files t)
+
+;;; 問い合せには y か n で返答
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;;; emacs permission
+;; (setq wdired-allow-to-change-permissions t)
+
+;;; diredの表示オプション
+(let ((gls (executable-find "gls")))
+ (when gls
+   (setq insert-directory-program gls
+         dired-listing-switches "-ahl --time-style long-iso --group-directories-first")))
+
+;;; emacs のデフォルトブラウザを eww に変更
+(setq browse-url-browser-function 'eww-browse-url)
+
+;;; Global Key
+;; copy & paste
+(global-set-key "\M-w" 'clipboard-kill-ring-save)
+(global-set-key "\C-w" 'clipboard-kill-region)
+
+(global-set-key "\C-cc"    'comment-or-uncomment-region)
+(global-set-key "\C-h"     'backward-delete-char)
+(global-set-key "\C-x\C-i" 'indent-region)
+(global-set-key [C-tab]    'indent-for-tab-command)
+
+(global-unset-key "\C-x\C-z")
+
+(when window-system
+  ;; Fonts
+  (add-to-list 'default-frame-alist '(font . "ricty-14"))
+
+  ;; フルスクリーン
+  (add-hook 'emacs-startup-hook #'toggle-frame-maximized)
+
+  ;; Modify right command to super
+  (setq mac-right-command-modifier 'super)
+
+  ;; フォントの拡大・縮小
+  (global-set-key (kbd "s-=") (lambda () (interactive) (text-scale-increase 1)))
+  (global-set-key (kbd "s--") (lambda () (interactive) (text-scale-decrease 1)))
+  (global-set-key (kbd "s-0") (lambda () (interactive) (text-scale-increase 0)))
+  )
 
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (file-exists-p custom-file)
